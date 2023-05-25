fb71abf40ff6881d739d41afd9c2ad3932eb04b8
Andreea <andelenasamur@gmail.com>
2023-05-25 17:55:50 +0200

    set min window size

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,60 @@
+import 'dart:io';
+import 'package:desktop_window/desktop_window.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (!kIsWeb && (Platform.isMacOS || Platform.isLinux || Platform.isWindows)) {
+    await DesktopWindow.setMinWindowSize(const Size(600, 800));
+  }
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Spotify Clone',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData(
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.black),
+        scaffoldBackgroundColor: const Color(0xFF121212),
+        primaryColor: Colors.black,
+        colorScheme: ColorScheme.fromSwatch(
+                primarySwatch: Colors.grey, brightness: Brightness.dark)
+            .copyWith(
+              background: const Color(0xFF121212),
+            )
+            .copyWith(
+              secondary: const Color(0xFF1DB954),
+            ),
+        iconTheme: const IconThemeData().copyWith(color: Colors.white),
+        fontFamily: 'Montserrat',
+        textTheme: const TextTheme(
+            displayMedium: TextStyle(
+              color: Colors.white,
+              fontSize: 32.0,
+              fontWeight: FontWeight.bold,
+            ),
+            headlineMedium: TextStyle(
+              fontSize: 12.0,
+              color: Color(0xFFE0E0E0),
+              fontWeight: FontWeight.w500,
+              letterSpacing: 2.0,
+            ),
+            bodyLarge: TextStyle(
+              color: Color(0xFFE0E0E0),
+              fontSize: 14.0,
+              fontWeight: FontWeight.w600,
+              letterSpacing: 1.0,
+            ),
+            bodyMedium: TextStyle(
+              color: Color(0xFFE0E0E0),
+              letterSpacing: 1.0,
+            )),
       ),
       home: Shell(),
     );
@@ -32,6 +64,28 @@ class MyApp extends StatelessWidget {
 class Shell extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Container(
+                  height: double.infinity,
+                  width: 280.0,
+                  color: Colors.green,
+                ),
+                // PlaylistScreen
+              ],
+            ),
+          ),
+          Container(
+            height: 84.0,
+            width: double.infinity,
+            color: Colors.blue,
+          ),
+        ],
+      ),
+    );
   }
 }
